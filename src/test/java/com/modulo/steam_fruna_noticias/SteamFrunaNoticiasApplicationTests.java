8b82b6f2e09bc0f0c6aea36ddd32eb6b5e38a8e8
Roberto Illesca <robertillesca1@gmail.com>
2023-12-06 11:59:52 -0300

    spring initialzr

---
+++ src/test/java/com/modulo/steam_fruna_noticias/SteamFrunaNoticiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modulo.steam_fruna_noticias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamFrunaNoticiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
